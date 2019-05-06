8c1c41f6b91dd4904c02a9f0a4817d499a33e137
Eva Z <dundunmao@gmail.com>
2019-05-06 15:38:39 -0700

    add fb

--- Google/replace_string1_to_string2.py
+++ Google/replace_string1_to_string2.py
@@ -9,6 +9,7 @@ def replace(s1, s2):
             if table[s1[i]] != s2[i]:
                 return False
 
+
 def replace2(s1, s2):
     if len(s1) != len(s2):
         return False
@@ -19,4 +20,15 @@ def replace2(s1, s2):
         else:
             if table[s1[i]] != s2[i]:
                 return False
+    stack1 = []
+    stack2 = []
+    for ele in s2:
+        if ele in table:
+            stack1.append(ele)
+        else:
+            stack2.append(ele)
+    res = stack2 + stack1
+    return set(res)
 
+if __name__ == "__main__":
+    print(replace2('aabcc', 'ccdee'))
